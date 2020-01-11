6113bb78a21d22787596dfb9dd9c395d30200dad
UNNO Hideyuki <unno.hideyuki@nifty.com>
2020-01-12 01:07:45 +0900

    Primes and ABC149C

--- abc149c/abc149c.hs
+++ abc149c/abc149c.hs
@@ -1,11 +1,14 @@
-module Primes where
-
 import           Control.Monad
 import qualified Control.Monad.ST            as ST
+import qualified Data.ByteString.Char8       as BS
+import           Data.List
+import           Data.Maybe
 import qualified Data.Vector.Unboxed         as V
 import qualified Data.Vector.Unboxed.Mutable as VM
 
--- generates a vector of prime numbers <= n
+readInt = fst . fromJust . BS.readInt
+getInt = readInt <$> BS.getLine
+
 primes n = ST.runST $ do
   v <- VM.new (n+1)
   VM.write v 0 0
@@ -18,3 +21,6 @@ primes n = ST.runST $ do
   v' <- V.freeze v
   return $ V.filter (> 0) v'
 
+main = do
+  x <- getInt
+  print $ V.head $ V.dropWhile (< x) $ primes 100003
