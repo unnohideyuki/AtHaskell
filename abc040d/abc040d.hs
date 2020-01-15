f4d7c17329c241896710c3b8ba8cc8c196f57f01
unnohideyuki <unno.hideyuki@nifty.com>
2020-01-15 18:40:40 +0900

    abc040d.hs

--- abc040d/abc040d.hs
+++ abc040d/abc040d.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 import           Control.Monad
 import           Control.Monad.Primitive
 import qualified Control.Monad.ST            as ST
@@ -6,6 +7,7 @@ import           Data.List
 import           Data.Maybe
 import qualified Data.Vector.Unboxed         as V
 import qualified Data.Vector.Unboxed.Mutable as VM
+
 
 newUF :: PrimMonad m => Int -> m (VM.MVector (PrimState m) Int)
 newUF n = do
@@ -51,17 +53,57 @@ readIntList = map readInt . BS.words
 getInt = readInt <$> BS.getLine
 getIntList = readIntList <$> BS.getLine
 
+-- quickSortVec from https://gist.github.com/kazu-yamamoto/3051375
+-- quickSortVec :: (Ord a, Unbox a) => [a] -> [a]
+quickSortList xs = ST.runST $ do
+  arr <- V.unsafeThaw $ V.fromList xs
+  let beg = 0
+      end = VM.length arr - 1
+  quickSortVec beg end arr
+  V.toList <$> V.unsafeFreeze arr
+
+quickSortVec l u arr
+  | l >= u    = return ()
+  | otherwise = do
+      VM.unsafeSwap arr l ((u+l)`div`2)
+      t <- VM.unsafeRead arr l
+      let i = l
+          j = u + 1
+      nj <- loopVec t u i j arr
+      VM.unsafeSwap arr l nj
+      quickSortVec l (nj-1) arr
+      quickSortVec (nj+1) u arr
+
+loopVec !t !u !i !j arr = do
+  ni <- doWhile i (+1)         (< t)
+  nj <- doWhile j (subtract 1) (> t)
+  if ni > nj
+    then return nj
+    else do VM.unsafeSwap arr ni nj
+            loopVec t u ni nj arr
+              where
+                {-# INLINE doWhile #-}
+                doWhile k op p
+                  | nk > u    = return nk
+                  | otherwise = do
+                      x <- VM.unsafeRead arr nk
+                      if p x then
+                        doWhile nk op p
+                        else
+                        return nk
+                        where
+                          nk = op k
 
 main = do
   [n, m] <- getIntList
   t <- replicateM m $ do [a, b, y] <- getIntList
                          return ((-y), (a, b))
-  let yabs = sortOn fst t
+  let yabs = quickSortList t
 
   q <- getInt
   t' <- forM [0..(q-1)] $ \i -> do [v, w] <- getIntList
                                    return ((-w), (i, v))
-  let wvs = sortOn fst t'
+  let wvs = quickSortList t'
 
   ans <- VM.new q
   uf <- newUF (n+1)
