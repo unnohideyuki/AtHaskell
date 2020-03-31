18c42226bcf9ba0697ed1be671fe23d6966713a9
Hideyuki Unno <unno.hideyuki@nifty.com>
2020-03-31 10:11:25 +0900

    remove empty lines.

--- snippets/ModInv.hs
+++ snippets/ModInv.hs
@@ -13,14 +13,11 @@ modpow a n p =
                                   (if ((n .&. 1) == 1) then res * a `mod` p else res)
   in modpow' a n 1
 
-
 -- compute a^(-1) `mod` p
 modinv :: Int -> Int -> Int
 modinv a p = modpow a (p-2) p
-
 
 main = do
   forM_ [1..13] $ \i -> (print $ modinv i 13)
   -- it should be 123456789123456789
   print $ 678813585 * (modinv 100000 1000000007) `mod` 1000000007
-
