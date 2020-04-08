395beece65a8adeb63ebd81aaef4cb4a97c7026a
Unno Hideyuki <unno.hideyuki@nifty.com>
2020-04-09 08:22:31 +0900

    starter

---
+++ snippets/Main.hs
@@ -0,0 +1,24 @@
+import           Control.Monad
+import qualified Data.Array.IO               as IO
+import           Data.Bits
+import qualified Data.ByteString.Char8       as BS
+import           Data.Char
+import           Data.Maybe
+import qualified Data.Vector.Unboxed         as V
+import qualified Data.Vector.Unboxed.Mutable as VM
+
+readInt = fst . fromJust . BS.readInt
+readIntList = map readInt . BS.words
+getInt = readInt <$> BS.getLine
+getIntList = readIntList <$> BS.getLine
+getIntVec n = V.unfoldrN n (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
+
+readInteger = fst . fromJust . BS.readInteger
+readIntegerList = map readInteger . BS.words
+getInteger = readInteger <$> BS.getLine
+getIntegerList = readIntegerList <$> BS.getLine
+
+main = do
+  print ""
+
+
