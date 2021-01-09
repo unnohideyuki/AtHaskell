63550abab4c4cba73cfd61e8b2f14711b33cd429
UNNO Hideyuki <unno.hideyuki@nifty.com>
2021-01-10 00:11:17 +0900

    modified to use Intger.

--- snippets/ModInv.hs
+++ snippets/ModInv.hs
@@ -1,23 +1,15 @@
-module ModInv where
-
-import           Control.Monad
-import           Data.Bits
-
 -- compulte a^n `mod` p
 modpow :: Int -> Int -> Int -> Int
 modpow a n p =
-  let modpow' a n res | n == 0    = res
+  let modpow' :: Integer -> Integer -> Integer -> Integer
+      modpow' a n res | n == 0    = res
                       | otherwise =
-                          modpow' (a*a`mod`p)
+                          modpow' (a*a`mod`p')
                                   (n `shiftR` 1)
-                                  (if (n .&. 1) == 1 then res * a `mod` p else res)
-  in modpow' a n 1
+                                  (if (n .&. 1) == 1 then res * a `mod` p' else res)
+      p' = fromIntegral p
+  in fromInteger $ modpow' (fromIntegral a) (fromIntegral n) 1
 
 -- compute a^(-1) `mod` p
 modinv :: Int -> Int -> Int
 modinv a p = modpow a (p-2) p
-
-main = do
-  forM_ [1..13] $ \i -> print $ modinv i 13
-  -- it should be 123456789123456789
-  print $ 678813585 * modinv 100000 1000000007 `mod` 1000000007
