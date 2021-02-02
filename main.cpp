c7620d375603faadedf1df6cc195c9f97ccabda7
UNNO Hideyuki <unno.hideyuki@nifty.com>
2021-02-02 21:35:08 +0900

    added INT, chmin and chmax.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <atcoder/all>
+using namespace atcoder;
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const double pi = 3.14159265359;
+const ll INF = 1LL << 60;
+template<class T> inline bool chmin(T& a, T b){
+  if (a > b){
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T& a, T b){
+  if (a < b){
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+
+  return 0;
+}
